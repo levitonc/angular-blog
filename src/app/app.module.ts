d3ba82d84bf966b9e64764d1fdc3dcf04273036f
Léviton Lima Carvalho <levitonc@live.com>
2023-09-08 16:21:46 -0300

    Menu Title feito

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
+import { MenuTittleComponent } from './components/menu-tittle/menu-tittle.component';
+import { BigCardComponent } from './components/big-card/big-card.component';
+import { LittleCardComponent } from './components/little-card/little-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuBarComponent,
+    MenuTittleComponent,
+    BigCardComponent,
+    LittleCardComponent
   ],
   imports: [
     BrowserModule,
